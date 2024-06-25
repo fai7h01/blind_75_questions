9c2cc4352232d338c408085b0a056e93f0a57229
fai7h01 <l.varsimashvil10@gmail.com>
2024-06-25 22:26:01 +0400

    11. LinkedListCycle, detectCycle

--- src/easy/LinkedListCycle.java
+++ src/easy/LinkedListCycle.java
@@ -34,7 +34,24 @@ public class LinkedListCycle {
 
         return false;
 
+    }
 
+    static ListNode detectCycle(ListNode head){
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) break;
+        }
+        if (fast == null || fast.next == null) return null; //there is no cycle
+        while(head != slow){
+            head = head.next;
+            slow = slow.next;
+        }
+        return head;
     }
 
 }
